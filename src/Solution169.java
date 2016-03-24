7430d4c40d82ed11ae9168721b7ccfbd73811c5b
Ajay Sreenivasan <ajaysreenivasan@gmail.com>
2016-03-24 19:47:07 -0400

    Solved Problem #198.

--- src/Solution169.java
+++ src/Solution169.java
@@ -1,39 +1,40 @@
+
 /*
- * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ * Given an array of size n, find the majority element.
+ * The majority element is the element that appears more than Math.floor(n/2) times.
  * You may assume that the array is non-empty and the majority element always exist in the array.
  */
 
 import java.util.HashMap;
 
 public class Solution169 {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    }
+	}
 
-    public int majorityElement(int[] nums) {
-        if(nums.length == 1){
-            return nums[0];
-        }
-        
-        int majorityElement = 0;
-        HashMap<Integer, Integer> elementTable = new HashMap<>();
+	public int majorityElement(int[] nums) {
+		if (nums.length == 1) {
+			return nums[0];
+		}
 
-        for (int i = 0; i < nums.length; i++) {
-            if (elementTable.containsKey(nums[i])) {
-                elementTable.put(nums[i], elementTable.get(nums[i]) + 1);
-            }
-            else {
-                elementTable.put(nums[i], 1);
-            }
-        }
+		int majorityElement = 0;
+		HashMap<Integer, Integer> elementTable = new HashMap<>();
 
-        for (int key : elementTable.keySet()) {
-            if (elementTable.get(key) >= Math.floor(nums.length / 2)) {
-                majorityElement = key;
-                break;
-            }
-        }
+		for (int i = 0; i < nums.length; i++) {
+			if (elementTable.containsKey(nums[i])) {
+				elementTable.put(nums[i], elementTable.get(nums[i]) + 1);
+			} else {
+				elementTable.put(nums[i], 1);
+			}
+		}
 
-        return majorityElement;
-    }
+		for (int key : elementTable.keySet()) {
+			if (elementTable.get(key) >= Math.floor(nums.length / 2)) {
+				majorityElement = key;
+				break;
+			}
+		}
+
+		return majorityElement;
+	}
 }
--- src/Solution198.java
+++ src/Solution198.java
@@ -1,13 +1,41 @@
 /*
- * Write a program to check whether a given number is an ugly number.
- * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
- * For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
- * Note that 1 is typically treated as an ugly number.
- */
-public class Solution198 {
-	public boolean isUgly(int num) {
-		boolean isUgly = false;
+ * You are a professional robber planning to rob houses along a street. 
+ * Each house has a certain amount of money stashed, the only constraint 
+ * stopping you from robbing each of them is that adjacent houses 
+ * have security system connected and it will automatically contact 
+ * the police if two adjacent houses were broken into on the same night.
+ * 
+ * Given a list of non-negative integers representing the amount of money of each house, 
+ * determine the maximum amount of money you can rob tonight without alerting the police.
+*/
 
-		return isUgly;
+public class Solution198 {
+	public int rob(int[] nums) {
+		int numsLength = nums.length;
+		int sum[] = new int[numsLength];
+		int maxMoney = 0;
+
+        if(nums == null || numsLength == 0){
+            return 0;
+        }
+
+		if (numsLength == 1) {
+			maxMoney = nums[0];
+		} else if (numsLength == 2) {
+			maxMoney = Math.max(nums[0], nums[1]);
+		} else {
+		    sum[0] = nums[0];
+		    sum[1] = Math.max(nums[0], nums[1]);
+		    
+			for (int i = 2; i < numsLength; i++) {
+				sum[i] = Math.max(sum[i - 2] + nums[i], sum[i - 1]);
+
+				if (maxMoney < sum[i]) {
+					maxMoney = sum[i];
+				}
+			}
+		}
+
+		return maxMoney;
 	}
 }
